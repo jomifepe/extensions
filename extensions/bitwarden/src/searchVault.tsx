6ff0bda56c46dd574b3332506af8da2d8c8ce8af
José Pereira <jmfp-b@live.com.pt>
2023-03-08 00:34:43 +0000

    refactor(vault): share selected vault item across actions using context

--- extensions/bitwarden/src/searchVault.tsx
+++ extensions/bitwarden/src/searchVault.tsx
@@ -1,7 +1,7 @@
 import { List, Icon, ActionPanel } from "@raycast/api";
 import RootErrorBoundary from "~/components/RootErrorBoundary";
 import SearchCommonActions from "~/components/search/actions/CommonActions";
-import SearchItem from "~/components/search/Item";
+import VaultItem from "~/components/search/Item";
 import { BitwardenProvider } from "~/context/bitwarden";
 import { SessionProvider } from "~/context/session";
 import { useVault, VaultProvider } from "~/context/vault";
@@ -25,7 +25,7 @@ function SearchVaultComponent() {
   return (
     <List isLoading={isLoading}>
       {items.map((item) => (
-        <SearchItem key={item.id} item={item} folder={getItemFolder(folders, item)} />
+        <VaultItem key={item.id} item={item} folder={getItemFolder(folders, item)} />
       ))}
       {isLoading ? (
         <List.EmptyView icon={Icon.ArrowClockwise} title="Loading..." description="Please wait." />
